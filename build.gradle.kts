4e548ec9dbe39d3f704babffb8aadce5a97fa84a
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2026-01-31 09:55:06 +0100

    Update gradle to allow publishing to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,64 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "2.3.0"
+    id("org.jetbrains.dokka") version "2.1.0"
+    id("com.vanniktech.maven.publish") version "0.36.0"
 }
 
 group = "org.capturecoop"
-version = "1.9.6"
+version = "1.9.7"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation(kotlin("stdlib"))
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val dokkaHtml by tasks.getting(org.jetbrains.dokka.gradle.DokkaTask::class)
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(dokkaHtml.outputDirectory)
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates("org.capturecoop", "defaultdepot", version.toString())
+
+    pom {
+        name.set("DefaultDepot")
+        description.set("Default Depository of Utility Functions.")
+        inceptionYear.set("2022")
+        url.set("https://github.com/CaptureCoop/DefaultDepot/")
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://github.com/CaptureCoop/DefaultDepot/blob/main/LICENSE")
+                distribution.set("https://github.com/CaptureCoop/DefaultDepot/blob/main/LICENSE")
+            }
+        }
+        developers {
+            developer {
+                id.set("SvenWollinger")
+                name.set("Sven Wollinger")
+                url.set("https://wollinger.io")
+            }
+        }
+        scm {
+            url.set("https://github.com/CaptureCoop/DefaultDepot/")
+            connection.set("scm:git:git://github.com/CaptureCoop/DefaultDepot.git")
+            developerConnection.set("scm:git:ssh://git@github.com/CaptureCoop/DefaultDepot.git")
+        }
+    }
 }
